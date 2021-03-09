9599f44e2a87dba5d34bb974830bebeb0f779ad6
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-03-09 22:17:47 +0100

    -Added Test for the reactive form to validate the corrects grops - Added test for the addAsset method

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -1,25 +1,80 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { of } from "rxjs";
+import { NavigationService } from "src/app/services/navigation.service";
+import { RepositoryService } from "src/app/services/repository.service";
 
-import { FormComponent } from './form.component';
+import { FormComponent } from "./form.component";
+import { FormArray, ReactiveFormsModule } from "@angular/forms";
+class RepositoryServiceStub {
+	savePins() {
+		return of(true);
+	}
+}
+class NavigationServiceStub {
+	goToPins() {}
+}
+class MatSnackBarStub {
+	open() {
+		return {
+			afterDismissed: () => {
+				return of(true);
+			},
+		};
+	}
+}
 
-describe('FormComponent', () => {
-  let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+describe("FormComponent", () => {
+	let component: FormComponent;
+	let fixture: ComponentFixture<FormComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [FormComponent],
+			providers: [
+				{ provide: RepositoryService, useClass: RepositoryServiceStub },
+				{ provide: NavigationService, useClass: NavigationServiceStub },
+				{ provide: MatSnackBar, useClass: MatSnackBarStub },
+			],
+			schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+			imports: [ReactiveFormsModule],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(FormComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	fdescribe("When the component is initialized", () => {
+		it("should create", () => {
+			expect(component).toBeTruthy();
+		});
+
+		it("Should create the forms", () => {
+			//console.log(component.firstFormGroup.controls);
+			expect(Object.keys(component.firstFormGroup.controls)).toEqual([
+				"title",
+				"author",
+				"description",
+			]);
+			expect(Object.keys(component.secondFormGroup.controls)).toEqual([
+				"firstAsset",
+				"assets",
+			]);
+		});
+	});
+
+	fdescribe("When addAsset is executed", () => {
+		it("should Add new group to the aray", () => {
+			const assets = <FormArray>component.secondFormGroup.get("assets");
+
+			component.addAsset();
+			component.addAsset();
+
+			expect(Object.keys(assets.controls)).toEqual(["0", "1"]);
+		});
+	});
 });
