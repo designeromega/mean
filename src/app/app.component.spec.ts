d16cc226a665df2002de866dd69be4c53471bef1
Anderson E. Peñaloza C <anderson.penaloza@antartyca.com>
2021-03-11 16:21:03 +0100

    Finished testes in layaout component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
-import { TestBed, async } from "@angular/core/testing";
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
+import { By } from "@angular/platform-browser";
+import { RouterOutlet } from "@angular/router";
 
 fdescribe("AppComponent", () => {
+	let fixture: ComponentFixture<AppComponent>;
+	let component: AppComponent;
+
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			imports: [RouterTestingModule],
@@ -10,24 +15,20 @@ fdescribe("AppComponent", () => {
 		}).compileComponents();
 	}));
 
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AppComponent);
+		component = fixture.debugElement.componentInstance;
+	});
+
 	it("should create the app", () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
 		expect(app).toBeTruthy();
 	});
 
-	it(`should have as title 'pin'`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app.title).toEqual("pin");
-	});
+	it(`should have a router Outlet`, () => {
+		const de = fixture.debugElement.query(By.directive(RouterOutlet));
 
-	it("should render title in a h1 tag", () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector("h1").textContent).toContain(
-			"Welcome to pin!"
-		);
+		expect(de).not.toBeNull();
 	});
 });
--- src/app/components/layout/layout.component.spec.ts
+++ src/app/components/layout/layout.component.spec.ts
@@ -1,25 +1,74 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatBottomSheet } from "@angular/material/bottom-sheet";
 
-import { LayoutComponent } from './layout.component';
+import { LayoutComponent } from "./layout.component";
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { RouterTestingModule } from "@angular/router/testing";
+import { FileDetector } from "selenium-webdriver";
+import { ActionsComponent } from "../actions/actions.component";
+class MatBottomSheetStub {
+	open() {}
+}
 
-describe('LayoutComponent', () => {
-  let component: LayoutComponent;
-  let fixture: ComponentFixture<LayoutComponent>;
+fdescribe("LayoutComponent", () => {
+	let component: LayoutComponent;
+	let fixture: ComponentFixture<LayoutComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [LayoutComponent],
+			providers: [{ provide: MatBottomSheet, useClass: MatBottomSheetStub }],
+			imports: [
+				RouterTestingModule.withRoutes([
+					{ path: "", component: LayoutComponent },
+					{ path: "app/add", component: LayoutComponent },
+				]),
+			],
+			schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LayoutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(LayoutComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
+
+	it("should set the edit mode to false", async(() => {
+		const verifyEditmode = spyOn(component, "verifyEditMode").and.callThrough();
+
+		fixture.ngZone.run(() => {
+			(<any>component).router.navigate(["/"]);
+
+			fixture.whenStable().then(() => {
+				expect(component.editMode).toBeFalsy();
+				expect(verifyEditmode).toHaveBeenCalled();
+			});
+		});
+	}));
+
+	it("should set the edit mode to true", async(() => {
+		const verifyEditmode = spyOn(component, "verifyEditMode").and.callThrough();
+
+		fixture.ngZone.run(() => {
+			(<any>component).router.navigate(["/app/add"]);
+
+			fixture.whenStable().then(() => {
+				expect(component.editMode).toBeTruthy();
+				expect(verifyEditmode).toHaveBeenCalled();
+			});
+		});
+	}));
+
+	it("should call openBottomSheet", () => {
+		const open = spyOn((<any>component).bottomSheet, "open");
+
+		component.openBottomSheet();
+
+		expect(open).toHaveBeenCalledWith(ActionsComponent);
+	});
 });
